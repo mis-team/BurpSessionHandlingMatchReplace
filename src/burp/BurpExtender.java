64aa0b2f3974bb620d826a6ffb0cb56952c805c1
M13 Information Security <mis@m13.su>
2021-05-11 17:26:45 -0400

    add autoload/autosave functionality, add import from text file

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -3,17 +3,31 @@ package burp;
 import java.awt.Component;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab
 {
 	private IExtensionHelpers helpers;
 	private MatchReplaceConfigurationPanel configuration;
+	//private Config config;
+	private IBurpExtenderCallbacks callbacks;
 
 	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+		this.callbacks = callbacks;
+		//this.config = new Config(this);
 		callbacks.registerSessionHandlingAction(this);
 		helpers = callbacks.getHelpers();
-		configuration = new MatchReplaceConfigurationPanel();
+		configuration = new MatchReplaceConfigurationPanel(this);
 		callbacks.customizeUiComponent(configuration);
 		callbacks.addSuiteTab(this);
+	}
+
+	public MatchReplaceConfigurationPanel getConfig() {
+		return this.configuration;
+	}
+
+	public IBurpExtenderCallbacks getCallbacks() {
+		return this.callbacks;
 	}
 
 	@Override
@@ -24,6 +38,7 @@ public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab
 	@Override
 	public void performAction(IHttpRequestResponse currentRequest, IHttpRequestResponse[] macroItems) {
 		String request = helpers.bytesToString(currentRequest.getRequest());
+		//List<MatchReplace> data = configuration.getData();
 		List<MatchReplace> data = configuration.getData();
 		for (MatchReplace element : data) {
 			request = request.replaceAll(element.getMatch(), element.getReplace());
--- src/burp/MatchReplaceConfigurationPanel.java
+++ src/burp/MatchReplaceConfigurationPanel.java
@@ -1,5 +1,12 @@
 package burp;
 
+//import com.google.common.collect.Lists;
+
+import com.google.gson.Gson;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
@@ -12,6 +19,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -23,6 +34,10 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+
 
 @SuppressWarnings("serial")
 public class MatchReplaceConfigurationPanel extends JPanel {
@@ -32,9 +47,14 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 	private static final int MATCH_COL = 1;
 	private static final int REPLACE_COL = 2;
 	private DefaultTableModel dataModel;
+	private BurpExtender burpExtender;
+	private IBurpExtenderCallbacks callbacks;
+	private JTextField configFile = new JTextField("", 60);
 	
-	public MatchReplaceConfigurationPanel() {
+	public MatchReplaceConfigurationPanel(BurpExtender burpExtender) {
 		setLayout(new BorderLayout(0, 0));
+		this.burpExtender = burpExtender;
+		this.callbacks = burpExtender.getCallbacks();
 		prepareTable();
 		prepareHeader();
 		prepareButtons();
@@ -48,7 +68,11 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 		}
 		return data;
 	}
-	
+
+	public void saveMatchReplaceTableData(String jsonData) {
+		this.callbacks.saveExtensionSetting("MatchReplaceTableData", jsonData);
+	}
+
 	private void prepareTable() {
 		table = new JTable();
 		
@@ -111,11 +135,117 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 	
 	private void prepareButtons() {
 		JPanel panel_1 = new JPanel();
+		JPanel panel_2 = new JPanel();
+		//JTextField configFile = new JTextField("", 60);
+
+
+		add(panel_2,BorderLayout.NORTH);
+		panel_2.add(new JLabel("Load text file ( split by : tab or comma )"));
+		//panel_2.add(configFile);
+
 		add(panel_1, BorderLayout.WEST);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.PAGE_AXIS));
+
+
 		prepareAddButton(panel_1);
 		prepareEditButton(panel_1);
 		prepareRemoveButton(panel_1);
+		prepareSaveButton(panel_1);
+		prepareLoadButton(panel_2);
+		loadConfig();
+	}
+
+	private void loadConfig(){
+		try {
+			List<MatchReplace> loadedData = new ArrayList<>();
+			String MatchReplaceTableData = this.callbacks.loadExtensionSetting("MatchReplaceTableData");
+			if (MatchReplaceTableData == null || MatchReplaceTableData.isEmpty()) {
+
+			}
+			else {
+				loadedData = new Gson().fromJson(MatchReplaceTableData, new TypeToken<List<MatchReplace>>() {
+				}.getType());
+				int rowCount = loadedData.size();
+				for (int i = 0; i < rowCount; i++) {
+					//data.add(new MatchReplace((String) table.getValueAt(i, MATCH_COL), (String) table.getValueAt(i, REPLACE_COL)));
+					int position =i + 1;
+					dataModel.addRow(new Object[] { position, loadedData.get(i).getMatch(), loadedData.get(i).getReplace() });
+				}
+
+
+				//saveMatchReplaceTableData(new Gson().toJson(this.getData()));
+				//this.callbacks.printOutput("Saved 2!");
+			}
+		}
+		catch (RuntimeException e) {
+			this.callbacks.printError(e.toString());
+			//this.callbacks.printOutput(e.toString());
+		}
+	}
+
+	private void saveConfig(){
+		try {
+
+			saveMatchReplaceTableData(new Gson().toJson(this.getData()));
+			//this.callbacks.printOutput("Saved 2!");
+		}
+		catch (RuntimeException e) {
+			this.callbacks.printError(e.toString());
+			//this.callbacks.printOutput(e.toString());
+		}
+	}
+
+	private void prepareLoadButton(JPanel panel_1) {
+		JButton btnLoad = new JButton("Load");
+		btnLoad.addActionListener((event) -> {
+
+			JFileChooser fc = new JFileChooser();
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			int returnVal = fc.showOpenDialog(frame);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				try {
+					BufferedReader input = new BufferedReader(new InputStreamReader(
+							new FileInputStream(file)));
+					String line;
+					dataModel.setRowCount(0);
+					int i = 1;
+					while( (line = input.readLine()) != null) {
+						if (line.matches(".*\\t.*")) {
+							dataModel.addRow(new Object[]{i, line.split("\\t")[0], line.split("\\t")[1]});
+							i++;
+						}
+						if (line.matches(".*,.*")) {
+							dataModel.addRow(new Object[]{i, line.split(",")[0], line.split(",")[1]});
+							i++;
+						}
+						if (line.matches(".*:.*")) {
+							dataModel.addRow(new Object[]{i, line.split(":")[0], line.split(":")[1]});
+							i++;
+						}
+					}
+				saveConfig();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("Operation is CANCELLED :(");
+			}
+
+
+			//this.burpExtender.getConfig().saveAutoDropRequestTableData(new Gson().toJson(autoDropRequestTable.getAutoDropRequestRules()));
+		});
+		panel_1.add(btnLoad);
+	}
+
+	private void prepareSaveButton(JPanel panel_1) {
+		JButton btnSave = new JButton("Save");
+		btnSave.addActionListener((event) -> {
+			saveConfig();
+		});
+		panel_1.add(btnSave);
 	}
 
 	private void prepareRemoveButton(JPanel panel_1) {
@@ -127,6 +257,7 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 			for (int i = 0; i < rowCount; i++) {
 				dataModel.setValueAt(i + selectedRow + 1, i + selectedRow, INDEX_COL);
 			}
+			saveConfig();
 		});
 		panel_1.add(btnRemove);
 	}
@@ -146,6 +277,7 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 				table.setValueAt(match.getText(), selectedRow, MATCH_COL);
 				table.setValueAt(replace.getText(), selectedRow, REPLACE_COL);
 			}
+			saveConfig();
 		});
 		panel_1.add(btnEdit);
 	}
@@ -164,6 +296,7 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 				int position = dataModel.getRowCount() + 1;
 				dataModel.addRow(new Object[] { position, match.getText(), replace.getText() });
 			}
+			saveConfig();
 		});
 		panel_1.add(btnAdd);
 	}
